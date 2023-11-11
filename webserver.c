a6103b2e3dd3f91986e37a6676f6ed4d88050360
Mahmoud Embaby <mah.embaby@outlook.com>
2023-11-11 17:26:11 +0200

    demo setup

---
+++ webserver.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080 // Convert to argument
+
+int main(int argc, char const* argv[]){    
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0}; // Buffer for incoming messages
+    char *hello = "Hello from server"; // Message to send to client
+
+    if ((server_fd == socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET; // IPv4
+    address.sin_addr.s_addr = INADDR_ANY; // Accept any incoming messages
+    address.sin_port = htons(PORT); // Convert to network byte order
+
+    if (bind(server_fd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    valread = read(new_socket, buffer, 1024 - 1); // Read message from client
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0); // Send message to client
+    printf("Hello message sent\n");
+
+    close(new_socket);
+    close(server_fd);
+    return 0;   
+}  
