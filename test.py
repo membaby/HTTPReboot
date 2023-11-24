314a9b71b8122a577ab07b55e22d3ceca182adcc
Mahmoud Embaby <mah.embaby@outlook.com>
2023-11-24 05:10:21 +0200

    Multi threading, GET and POST handling

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
 import socket
+import concurrent.futures
 
-def main():
-    host = '127.0.0.1'
-    port = 8080
+def make_request(host, port, method, path='/', body=None):
+
+    if method == 'GET':
+        request = f'GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n'
+    elif method == 'POST':
+        if body is None:
+            raise ValueError('Body must be provided for POST requests')
+        request = f'POST {path} HTTP/1.1\r\nHost: {host}\r\nContent-Length: {len(body)}\r\n\r\n{body}'
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        message = 'Hello from Python client'
-        s.sendall(message.encode())
-
-        data = s.recv(1024);
-        print('Received', repr(data))
+        s.sendall(request.encode())
+        data = s.recv(4096);
+        print('Received:', data.decode())
 
 if __name__ == '__main__':
-    main()
+    host = '127.0.0.1'
+    port = 8080
+    
+    # Sending a GET request
+    # make_request(host, port, 'GET', 'file.txt', None)
+
+    # Sending a POST request
+    make_request(host, port, 'POST', 'file.txt', 'Hello from Python client')
+
+    # executor = concurrent.futures.ThreadPoolExecutor(max_workers=32)
+    # futures = [executor.submit(make_request) for i in range(100)]
+    # concurrent.futures.wait(futures)
--- webserver.c
+++ webserver.c
@@ -4,11 +4,81 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define DEFAULT_PORT 8080
 
+void *connection_handler(void *socket){
+    int client_socket = *(int*)socket;
+    free(socket); // Free memory allocated for socket descriptor
+    char buffer[1024] = {0}; // Buffer for incoming messages
+    char *hello = "Hello from server"; // Message to send to client
+
+    // Read message from client
+    int valread = read(client_socket, buffer, 1024 - 1);
+    if (valread < 0) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+    buffer[valread] = '\0'; // Null terminate string
+    printf("Message from client: %s\n", buffer);
+
+    // Print message from client
+    char method[10], path[1024], protocol[10];
+    sscanf(buffer, "%s %s %s", method, path, protocol);
+
+    printf("Path: %s\n", path);
+    // printf("Protocol: %s\n", protocol);
+
+    if (strcmp(method, "GET") == 0) {
+
+        FILE *file = fopen(path, "r");
+        // FILE *file = fopen("file.txt", "r");
+        if (file == NULL) {
+            const char *notFoundMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
+            send(client_socket, notFoundMessage, strlen(notFoundMessage), 0);
+        } else {
+            // Read file content
+            fseek(file, 0, SEEK_END);
+            long fsize = ftell(file);
+            fseek(file, 0, SEEK_SET);
+            char *data = malloc(fsize + 1);
+            fread(data, 1, fsize, file);
+            fclose(file);
+            data[fsize] = 0;
+
+            // Send file content to client
+            char header[1024];
+            sprintf(header, "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\n\r\n", fsize);
+            send(client_socket, header, strlen(header), 0);
+            send(client_socket, data, fsize, 0);
+            free(data);
+        }
+    } else if (strcmp(method, "POST") == 0) {
+        const char *okMessage = "HTTP/1.1 200 OK\r\n\r\n";
+        send(client_socket, okMessage, strlen(okMessage), 0);
+
+        char fileBuffer[2048];  // Buffer for file content
+        int fileValRead = read(client_socket, fileBuffer, 2048 - 1);
+        if (fileValRead < 0) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        fileBuffer[fileValRead] = '\0'; // Null terminate string
+        printf("File content: %s\n", fileBuffer);
+
+    } else {
+        const char *notImplementedMessage = "HTTP/1.1 501 Not Implemented\r\n\r\n";
+        send(client_socket, notImplementedMessage, strlen(notImplementedMessage), 0);
+    }
+
+    // Source: After finishing the transmission, the server should keep the connection open waiting for new requests from the same client.
+    // Source: Close if connection timed out
+    return NULL;
+}
+
 int main(int argc, char const* argv[]){    
-    int server_fd, new_socket;    // File descriptors for server and client sockets
+    int server_fd;    // File descriptors for server and client sockets
     struct sockaddr_in address;   // Address structure for IPv4
     int opt = 1;                  // Option value for setsockopt
     int addrlen = sizeof(address);
@@ -42,30 +112,24 @@ int main(int argc, char const* argv[]){
         exit(EXIT_FAILURE);
     }
     
-    // Accept incoming connection
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    while(1) {
+        // Accept incoming connection
+        int *new_socket = malloc(sizeof(int));
+        if ((*new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            free(new_socket);
+            continue;
+        }
+
+        // Create thread to handle connection
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, connection_handler, (void*)new_socket) < 0) {
+            perror("pthread_create");
+            free(new_socket);
+            continue;
+        }
     }
 
-    // Read message from client and send response
-    int valread = read(new_socket, buffer, 1024 - 1);
-    if (valread < 0) {
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
-    buffer[valread] = '\0'; // Null terminate string
-    printf("Client: %s\n", buffer);
-
-    // Send message to client
-    if (send(new_socket, hello, strlen(hello), 0) < 0) {
-        perror("send");
-        exit(EXIT_FAILURE);
-    }
-    printf("Hello message sent\n");
-
-    // Close sockets
-    close(new_socket);
     close(server_fd);
 
     return 0;   
