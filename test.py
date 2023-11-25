410e919ef5ab8985630c859963335cf38854b36a
Mahmoud Embaby <mah.embaby@outlook.com>
2023-11-26 00:40:52 +0200

    Handling GET request

--- test.py
+++ test.py
@@ -13,18 +13,24 @@ def make_request(host, port, method, path='/', body=None):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(request.encode())
-        data = s.recv(4096);
-        print('Received:', data.decode())
+        response = b""
+        while True:
+            part = s.recv(4096)
+            if not part:
+                break
+            response += part
+        print('Received:', response.decode())
+
 
 if __name__ == '__main__':
     host = '127.0.0.1'
     port = 8080
     
     # Sending a GET request
-    # make_request(host, port, 'GET', 'file.txt', None)
+    make_request(host, port, 'GET', '/index.html', None)
 
     # Sending a POST request
-    make_request(host, port, 'POST', 'file.txt', 'Hello from Python client')
+    # make_request(host, port, 'POST', 'file.txt', 'Hello from Python client')
 
     # executor = concurrent.futures.ThreadPoolExecutor(max_workers=32)
     # futures = [executor.submit(make_request) for i in range(100)]
--- webserver.c
+++ webserver.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <time.h>
 
 #define DEFAULT_PORT 8080
 
@@ -12,28 +13,30 @@ void *connection_handler(void *socket){
     int client_socket = *(int*)socket;
     free(socket); // Free memory allocated for socket descriptor
     char buffer[1024] = {0}; // Buffer for incoming messages
-    char *hello = "Hello from server"; // Message to send to client
 
     // Read message from client
-    int valread = read(client_socket, buffer, 1024 - 1);
+    int valread = read(client_socket, buffer, 1024 - 1); // RESEARCH RECV VS READ
     if (valread < 0) {
         perror("read");
         exit(EXIT_FAILURE);
     }
     buffer[valread] = '\0'; // Null terminate string
-    printf("Message from client: %s\n", buffer);
+
+    // Print Incoming Message
+    time_t now = time(NULL);
+    struct tm *tm_now = localtime(&now);
+    char time_str[100];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", tm_now);
+    printf("[%s] Incoming Request: %s\n", time_str, buffer);
 
     // Print message from client
-    char method[10], path[1024], protocol[10];
-    sscanf(buffer, "%s %s %s", method, path, protocol);
+    char method[10], protocol[10];
+    char path[1024] = "public";
+    sscanf(buffer, "%s %s %s", method, path+6, protocol);
 
-    printf("Path: %s\n", path);
-    // printf("Protocol: %s\n", protocol);
+    if (strncmp(method, "GET", 3) == 0) {
 
-    if (strcmp(method, "GET") == 0) {
-
-        FILE *file = fopen(path, "r");
-        // FILE *file = fopen("file.txt", "r");
+        FILE *file = fopen(path, "rb"); // opens file in binary mode to avoid problems with text files
         if (file == NULL) {
             const char *notFoundMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
             send(client_socket, notFoundMessage, strlen(notFoundMessage), 0);
@@ -42,7 +45,7 @@ void *connection_handler(void *socket){
             fseek(file, 0, SEEK_END);
             long fsize = ftell(file);
             fseek(file, 0, SEEK_SET);
-            char *data = malloc(fsize + 1);
+            char *data = calloc(fsize + 1, sizeof(char));
             fread(data, 1, fsize, file);
             fclose(file);
             data[fsize] = 0;
@@ -51,10 +54,10 @@ void *connection_handler(void *socket){
             char header[1024];
             sprintf(header, "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\n\r\n", fsize);
             send(client_socket, header, strlen(header), 0);
-            send(client_socket, data, fsize, 0);
+            send(client_socket, data, fsize, 0);       // RESEARCH SENDING 1 VS SENDING 2 MESSAGES
             free(data);
         }
-    } else if (strcmp(method, "POST") == 0) {
+    } else if (strncmp(method, "POST", 4) == 0) {
         const char *okMessage = "HTTP/1.1 200 OK\r\n\r\n";
         send(client_socket, okMessage, strlen(okMessage), 0);
 
@@ -65,7 +68,6 @@ void *connection_handler(void *socket){
             exit(EXIT_FAILURE);
         }
         fileBuffer[fileValRead] = '\0'; // Null terminate string
-        printf("File content: %s\n", fileBuffer);
 
     } else {
         const char *notImplementedMessage = "HTTP/1.1 501 Not Implemented\r\n\r\n";
@@ -74,6 +76,7 @@ void *connection_handler(void *socket){
 
     // Source: After finishing the transmission, the server should keep the connection open waiting for new requests from the same client.
     // Source: Close if connection timed out
+    close(client_socket); // REMOVE AFTER IMPLEMENTING TIMEOUT ALGORITHM # KEEP TO PREVENT MEMORY LEAKS
     return NULL;
 }
 
@@ -83,7 +86,6 @@ int main(int argc, char const* argv[]){
     int opt = 1;                  // Option value for setsockopt
     int addrlen = sizeof(address);
     char buffer[1024] = {0};      // Buffer for incoming messages
-    char *hello = "Hello from server"; // Message to send to client
 
     // Create socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -92,7 +94,7 @@ int main(int argc, char const* argv[]){
     }
 
     // Attach socket to port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) { // RESEARCH SO_REUSEADDR
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -107,14 +109,14 @@ int main(int argc, char const* argv[]){
     }
     
     // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0) {     // RESEARCH BACKLOG - WHAT IS THE BEST VALUE?
         perror("listen");
         exit(EXIT_FAILURE);
     }
     
     while(1) {
         // Accept incoming connection
-        int *new_socket = malloc(sizeof(int));
+        int *new_socket = calloc(1, sizeof(int));
         if ((*new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             free(new_socket);
